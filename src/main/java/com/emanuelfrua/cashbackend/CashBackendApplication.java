8a47295b8a2d6746e82e153cbf88851a50f26303
EmEr <emanuelfrua@gmail.com>
2021-06-17 18:10:33 -0300

    first commit

---
+++ src/main/java/com/emanuelfrua/cashbackend/CashBackendApplication.java
@@ -0,0 +1,13 @@
+package com.emanuelfrua.cashbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CashBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CashBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/emanuelfrua/cashbackend/CashBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emanuelfrua.cashbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CashBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
