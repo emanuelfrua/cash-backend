55adf42ff59c507c59468f9810f30e505fe2b5c7
EmEr <emanuelfrua@gmail.com>
2021-06-21 16:11:38 -0300

    Upload properties

--- src/main/java/com/emanuelfrua/cashbackend/CashBackendApplication.java
+++ src/main/java/com/emanuelfrua/cashbackend/CashBackendApplication.java
@@ -1,6 +1,8 @@
 package com.emanuelfrua.cashbackend;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
@@ -8,6 +10,7 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableJpaAuditing
+@EnableEncryptableProperties
 public class CashBackendApplication {
 
     public static void main(String[] args) {
